e200e4bb69e72017b4cb7ff8973efd508cd5c776
fiveham <fiveham@users.noreply.github.com>
2016-11-24 15:40:20 -0500

    move HTML-to-text operation into Operation class

--- src/operate/Operation.java
+++ src/operate/Operation.java
@@ -1,7 +1,12 @@
 package operate;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+
+import common.IO;
+import html.HTMLFile;
 
 //TODO update the javadoc for the methods that have had an args param added
 public enum Operation{
@@ -9,73 +14,73 @@ public enum Operation{
             null, 
             Folder.HTML_BOOKS_NEWLINE, 
             Folder.HTML_BOOKS, 
-            NewlineP::newlineP), 
+            (o,args,msg) -> NewlineP.newlineP(args, msg)), 
     CLEAR_EXCESS_STRUCTURE(
             null, 
             Folder.HTML_BOOKS_UNSTRUCTURED, 
             Folder.HTML_BOOKS_NEWLINE, 
-            ClearExcessStructure::clearXSStruct), 
+            (o,args,msg) -> ClearExcessStructure.clearXSStruct(args, msg)), 
     CLEAR_FRONT_AND_BACK_MATTER(
             null, 
             Folder.HTML_BOOKS_CHAPTER_CORE, 
             Folder.HTML_BOOKS_UNSTRUCTURED, 
-            ClearFrontAndBackMatter::clearFrontBack), 
+            (o,args,msg) -> ClearFrontAndBackMatter.clearFrontBack(args, msg)), 
     SWAP_APOSTROPHES(
             null, 
             Folder.HTML_BOOKS_CORRECT_APOSTROPHES, 
             Folder.HTML_BOOKS_CHAPTER_CORE, 
-            SwapApostrophes::swapApostrophes), 
+            (o,args,msg) -> SwapApostrophes.swapApostrophes(args, msg)), 
     SPLIT_CHAPTERS(
             null, 
             Folder.HTML_CHAPTERS, 
             Folder.HTML_BOOKS_CORRECT_APOSTROPHES, 
-            SplitChapters::splitChapters), 
+            (o,args,msg) -> SplitChapters.splitChapters(args, msg)), 
     HTML_TO_TEXT(
             null, 
             Folder.CORPUS, 
             Folder.HTML_CHAPTERS, 
-            HtmlToText::htmlToText), 
+            Operation::htmlToText), 
     FIND_REPEATED_PHRASES(
             null, 
             Folder.REPEATS, 
             Folder.CORPUS, 
-            FindRepeatedPhrases::findRepPhrases),
+            (o,args,msg) -> FindRepeatedPhrases.findRepPhrases(args, msg)),
     REMOVE_DEPENDENT_PHRASES(
             null, 
             Folder.INDEPENDENT_INSTANCES, 
             Folder.REPEATS, 
-            RemoveDependentPhrases::rmDepPhrases), 
+            (o,args,msg) -> RemoveDependentPhrases.rmDepPhrases(args, msg)), 
     REMOVE_UNIQUE_INDEPENDENTS(
             null, 
             Folder.DUPLICATE_INDEPENDENTS, 
             Folder.INDEPENDENT_INSTANCES, 
-            RemoveUniqueIndependents::rmUniqIndeps),
+            (o,args,msg) -> RemoveUniqueIndependents.rmUniqIndeps(args, msg)),
     DETERMINE_ANCHORS(
             null, 
             Folder.ANCHORS, 
             Folder.DUPLICATE_INDEPENDENTS, 
-            DetermineAnchors::determineAnchors), 
+            (o,args,msg) -> DetermineAnchors.determineAnchors(args, msg)), 
     LINK_CHAPTERS(
             Folder.ANCHORS, 
             Folder.LINKED_CHAPTERS, 
             Folder.HTML_CHAPTERS, 
-            LinkChapters::linkChapters), 
+            (o,args,msg) -> LinkChapters.linkChapters(args, msg)), 
     SET_TRAIL(
             null, 
             Folder.READABLE, 
             Folder.LINKED_CHAPTERS, 
-            SetTrail::setTrail);
-
+            (o,args,msg) -> SetTrail.setTrail(args, msg));
+    
     private final Folder readDecoration;
     private final Folder writeTo;
     private final Folder readFrom;
-    private final BiConsumer<String[], ? super Consumer<String>> operation;
+    private final TriConsumer<Operation, String[], ? super Consumer<String>> operation;
     
     private Operation(
             Folder readDecoration, 
             Folder writeTo, 
             Folder readFrom, 
-            BiConsumer<String[], ? super Consumer<String>> operation){
+            TriConsumer<Operation, String[], ? super Consumer<String>> operation){
         
         this.readDecoration = readDecoration;
         this.writeTo = writeTo;
@@ -96,10 +101,36 @@ public enum Operation{
     }
     
     public void operate(String[] args, Consumer<String> msg){
-        operation.accept(args, msg);
+        operation.accept(this, args, msg);
     }
     
     public BiConsumer<String[], ? super Consumer<String>> operation(){
-        return this.operation;
+        return this::operate;
+    }
+    
+    /**
+     * <p>Detects all the .html files in {@code READ_FROM}, reads them as HTMLFiles, and prints them
+     * as .txt files in {@code WRITE_TO}.</p>
+     * @param args command-line arguments (unused)
+     */
+    private static void htmlToText(Operation op, String[] args, Consumer<String> msg) {
+        File[] readUs = op.readFrom().folder().listFiles(IO::isHtml);
+        for(File f : readUs){
+            try{
+                msg.accept("Saving as text " + f.getName());
+                new HTMLFile(f).printAsText(
+                        op.writeTo().folderName() 
+                        + File.separator 
+                        + IO.stripExtension(f.getName()) 
+                        + IO.TXT_EXT);
+            } catch(FileNotFoundException e){
+                throw new RuntimeException(IO.ERROR_EXIT_MSG + f.getName() + " for reading");
+            }
+        }
+    }
+    
+    @FunctionalInterface
+    private interface TriConsumer<A,B,C>{
+        public void accept(A a, B b, C c);
     }
 }
