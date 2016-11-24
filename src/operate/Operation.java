04234e9e6739cd93be6735e16e67ca65f46675a7
fiveham <fiveham@users.noreply.github.com>
2016-11-24 11:34:53 -0500

    enfore 100-column limit

--- src/operate/Operation.java
+++ src/operate/Operation.java
@@ -7,37 +7,72 @@ import common.Folder;
 
 //TODO add a member that accepts a Consumer<String> and let it point to these classes' work methods
 //TODO update the javadoc for the methods that have had an args param added
-//TODO enforce 100-column limit here
 //TODO resequence these elements to reflect the folder order
 public enum Operation{
     CLEAR_EXCESS_STRUCTURE(
-            null,           Folder.HTML_BOOKS_UNSTRUCTURED, Folder.HTML_BOOKS_NEWLINE, ClearExcessStructure::clearXSStruct), 
+            null, 
+            Folder.HTML_BOOKS_UNSTRUCTURED, 
+            Folder.HTML_BOOKS_NEWLINE, 
+            ClearExcessStructure::clearXSStruct), 
     CLEAR_FRONT_AND_BACK_MATTER(
-            null,           Folder.HTML_BOOKS_CHAPTER_CORE, Folder.HTML_BOOKS_UNSTRUCTURED, ClearFrontAndBackMatter::clearFrontBack),
+            null, 
+            Folder.HTML_BOOKS_CHAPTER_CORE, 
+            Folder.HTML_BOOKS_UNSTRUCTURED, 
+            ClearFrontAndBackMatter::clearFrontBack),
     DETERMINE_ANCHORS(
-            null,           Folder.ANCHORS,                 Folder.DUPLICATE_INDEPENDENTS, DetermineAnchors::determineAnchors), 
+            null, 
+            Folder.ANCHORS, 
+            Folder.DUPLICATE_INDEPENDENTS, 
+            DetermineAnchors::determineAnchors), 
     FIND_REPEATED_PHRASES(
-            null,           Folder.REPEATS,                 Folder.CORPUS, FindRepeatedPhrases::findRepPhrases), 
+            null, 
+            Folder.REPEATS, 
+            Folder.CORPUS, 
+            FindRepeatedPhrases::findRepPhrases), 
     HTML_TO_TEXT(
-            null,           Folder.CORPUS,                  Folder.HTML_CHAPTERS, HtmlToText::htmlToText), 
+            null, 
+            Folder.CORPUS, 
+            Folder.HTML_CHAPTERS, 
+            HtmlToText::htmlToText), 
     LINK_CHAPTERS(
-            Folder.ANCHORS, Folder.LINKED_CHAPTERS,         Folder.HTML_CHAPTERS, LinkChapters::linkChapters), 
+            Folder.ANCHORS, 
+            Folder.LINKED_CHAPTERS, 
+            Folder.HTML_CHAPTERS, 
+            LinkChapters::linkChapters), 
     NEWLINE_P(
-            null,           Folder.HTML_BOOKS_NEWLINE,      Folder.HTML_BOOKS, NewlineP::newlineP), 
+            null, 
+            Folder.HTML_BOOKS_NEWLINE, 
+            Folder.HTML_BOOKS, 
+            NewlineP::newlineP), 
     REMOVE_DEPENDENT_PHRASES(
-            null,           Folder.INDEPENDENT_INSTANCES,   Folder.REPEATS, RemoveDependentPhrases::rmDepPhrases), 
+            null, 
+            Folder.INDEPENDENT_INSTANCES, 
+            Folder.REPEATS, 
+            RemoveDependentPhrases::rmDepPhrases), 
     REMOVE_UNIQUE_INDEPENDENTS(
-            null,           Folder.DUPLICATE_INDEPENDENTS,  Folder.INDEPENDENT_INSTANCES, RemoveUniqueIndependents::rmUniqIndeps), 
+            null, 
+            Folder.DUPLICATE_INDEPENDENTS, 
+            Folder.INDEPENDENT_INSTANCES, 
+            RemoveUniqueIndependents::rmUniqIndeps), 
     SET_TRAIL(
-            null,           Folder.READABLE,                Folder.LINKED_CHAPTERS, SetTrail::setTrail), 
+            null, 
+            Folder.READABLE, 
+            Folder.LINKED_CHAPTERS, 
+            SetTrail::setTrail), 
     SPLIT_CHAPTERS(
-            null,           Folder.HTML_CHAPTERS,           Folder.HTML_BOOKS_CORRECT_APOSTROPHES, SplitChapters::splitChapters), 
+            null, 
+            Folder.HTML_CHAPTERS, 
+            Folder.HTML_BOOKS_CORRECT_APOSTROPHES, 
+            SplitChapters::splitChapters), 
     SWAP_APOSTROPHES(
-            null,           Folder.HTML_BOOKS_CORRECT_APOSTROPHES, Folder.HTML_BOOKS_CHAPTER_CORE, SwapApostrophes::swapApostrophes);
-    
+            null, 
+            Folder.HTML_BOOKS_CORRECT_APOSTROPHES, 
+            Folder.HTML_BOOKS_CHAPTER_CORE, 
+            SwapApostrophes::swapApostrophes);
+
+    private final Folder readDecoration;
     private final Folder writeTo;
     private final Folder readFrom;
-    private final Folder readDecoration;
     private final BiConsumer<String[], ? super Consumer<String>> operation;
     
     private Operation(
@@ -46,9 +81,9 @@ public enum Operation{
             Folder readFrom, 
             BiConsumer<String[], ? super Consumer<String>> operation){
         
+        this.readDecoration = readDecoration;
         this.writeTo = writeTo;
         this.readFrom = readFrom;
-        this.readDecoration = readDecoration;
         this.operation = operation;
     }
     
