eff2b77dfaf8528e234831116a2935252f7f167c
fiveham <fiveham@users.noreply.github.com>
2016-11-30 20:33:47 -0500

    put constructor-args in from-to order

--- src/operate/Operation.java
+++ src/operate/Operation.java
@@ -12,79 +12,79 @@ import html.HTMLFile;
 public enum Operation{
     NEWLINE_P(
             null, 
-            Folder.HTML_BOOKS_NEWLINE, 
             Folder.HTML_BOOKS, 
+            Folder.HTML_BOOKS_NEWLINE, 
             (o,args,msg) -> NewlineP.newlineP(args, msg)), 
     CLEAR_EXCESS_STRUCTURE(
             null, 
-            Folder.HTML_BOOKS_UNSTRUCTURED, 
             Folder.HTML_BOOKS_NEWLINE, 
+            Folder.HTML_BOOKS_UNSTRUCTURED, 
             (o,args,msg) -> ClearExcessStructure.clearXSStruct(args, msg)), 
     CLEAR_FRONT_AND_BACK_MATTER(
             null, 
-            Folder.HTML_BOOKS_CHAPTER_CORE, 
             Folder.HTML_BOOKS_UNSTRUCTURED, 
+            Folder.HTML_BOOKS_CHAPTER_CORE, 
             (o,args,msg) -> ClearFrontAndBackMatter.clearFrontBack(args, msg)), 
     SWAP_APOSTROPHES(
             null, 
-            Folder.HTML_BOOKS_CORRECT_APOSTROPHES, 
             Folder.HTML_BOOKS_CHAPTER_CORE, 
+            Folder.HTML_BOOKS_CORRECT_APOSTROPHES, 
             (o,args,msg) -> SwapApostrophes.swapApostrophes(args, msg)), 
     SPLIT_CHAPTERS(
             null, 
-            Folder.HTML_CHAPTERS, 
             Folder.HTML_BOOKS_CORRECT_APOSTROPHES, 
+            Folder.HTML_CHAPTERS, 
             (o,args,msg) -> SplitChapters.splitChapters(args, msg)), 
     HTML_TO_TEXT(
             null, 
-            Folder.CORPUS, 
             Folder.HTML_CHAPTERS, 
+            Folder.CORPUS, 
             Operation::htmlToText), 
     FIND_REPEATED_PHRASES(
             null, 
-            Folder.REPEATS, 
             Folder.CORPUS, 
+            Folder.REPEATS, 
             (o,args,msg) -> FindRepeatedPhrases.findRepPhrases(args, msg)),
     REMOVE_DEPENDENT_PHRASES(
             null, 
-            Folder.INDEPENDENT_INSTANCES, 
             Folder.REPEATS, 
+            Folder.INDEPENDENT_INSTANCES, 
             (o,args,msg) -> RemoveDependentPhrases.rmDepPhrases(args, msg)), 
     REMOVE_UNIQUE_INDEPENDENTS(
             null, 
-            Folder.DUPLICATE_INDEPENDENTS, 
             Folder.INDEPENDENT_INSTANCES, 
+            Folder.DUPLICATE_INDEPENDENTS, 
             (o,args,msg) -> RemoveUniqueIndependents.rmUniqIndeps(args, msg)),
     DETERMINE_ANCHORS(
             null, 
-            Folder.ANCHORS, 
             Folder.DUPLICATE_INDEPENDENTS, 
+            Folder.ANCHORS, 
             (o,args,msg) -> DetermineAnchors.determineAnchors(args, msg)), 
     LINK_CHAPTERS(
             Folder.ANCHORS, 
-            Folder.LINKED_CHAPTERS, 
             Folder.HTML_CHAPTERS, 
+            Folder.LINKED_CHAPTERS, 
             (o,args,msg) -> LinkChapters.linkChapters(args, msg)), 
     SET_TRAIL(
             null, 
-            Folder.READABLE, 
             Folder.LINKED_CHAPTERS, 
+            Folder.READABLE, 
             (o,args,msg) -> SetTrail.setTrail(args, msg));
     
     private final Folder readDecoration;
-    private final Folder writeTo;
     private final Folder readFrom;
+    private final Folder writeTo;
     private final TriConsumer<Operation, String[], ? super Consumer<String>> operation;
     
     private Operation(
             Folder readDecoration, 
-            Folder writeTo, 
             Folder readFrom, 
+            Folder writeTo, 
             TriConsumer<Operation, String[], ? super Consumer<String>> operation){
         
         this.readDecoration = readDecoration;
-        this.writeTo = writeTo;
         this.readFrom = readFrom;
+        this.writeTo = writeTo;
         this.operation = operation;
     }
     
