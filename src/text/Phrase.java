ca5cd6f37a9271ab4e041df1da37acb7e2b5f9ad
fiveham <fiveham@users.noreply.github.com>
2016-12-04 19:28:34 -0500

    refactor reduced()
    add javadoc

--- src/text/Phrase.java
+++ src/text/Phrase.java
@@ -1,26 +1,62 @@
 package text;
 
+/**
+ * <p>Represents a phrase found in the text of the novels and novellas of A Song of Ice and 
+ * Fire.</p>
+ * @author fiveham
+ *
+ */
 public class Phrase {
+    
+    /**
+     * <p>An empty string. This is the {@link #reduced() reduced} form of a phrase with only one 
+     * word.</p>
+     */
+    public static final String ZERO_WORD_PHRASE = "";
+    
+    /**
+     * <p>The value that should be specified as {@code lastSpace} for a phrase with one word.</p>
+     */
+    public static final int LAST_SPACE_INDEX_FOR_ONE_WORD_PHRASE = -1;
     
     private final String text;
     private final int lastSpace;
     
+    /**
+     * <p>Constructs a Phrase for the specified piece of {@code text}
+     * @param text the actual phrase from the text of a book
+     * @param lastSpace the {@link String#lastIndexOf(String) last index of} a space in {@code text}
+     */
     Phrase(String text, int lastSpace){
         this.text = text;
         this.lastSpace = lastSpace;
     }
     
+    /**
+     * <p>Returns the index of the last space ({@code " "}) in the {@link #getText() actual phrase} 
+     * of this Phrase.</p>
+     * @return the index of the last space ({@code " "}) in the {@link #getText() actual phrase} of 
+     * this Phrase
+     */
     public int getLastSpace(){
         return lastSpace;
     }
     
+    /**
+     * <p>Returns the actual phrase this object wraps.</p>
+     * @return this phrase without the last word
+     */
     public String getText(){
         return text;
     }
     
-    String reduced = null;
-    
+    /**
+     * <p>Returns this phrase without the last word.</p>
+     * @return this phrase without the last word
+     */
     public String reduced(){
-        return text.substring(0, lastSpace);
+        return lastSpace == LAST_SPACE_INDEX_FOR_ONE_WORD_PHRASE 
+                ? ZERO_WORD_PHRASE 
+                : text.substring(0, lastSpace);
     }
 }
