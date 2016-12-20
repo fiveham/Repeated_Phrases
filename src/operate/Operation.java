4951e0bea9a3186f1312894d07a8275b413de712
fiveham <fiveham@users.noreply.github.com>
2016-12-20 16:02:39 -0500

    remove unused rmUniqIndeps method

--- src/operate/Operation.java
+++ src/operate/Operation.java
@@ -1,13 +1,6 @@
 package operate;
 
-import common.IO;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
 import java.util.function.Consumer;
-import java.util.stream.IntStream;
-import text.Location;
 
 //TODO describe what the inputs and outputs are for each Operation
 //in preparation to integrate the operations
@@ -92,41 +85,6 @@ public enum Operation{
     
     public void operate(String[] args, Consumer<String> msg){
         operation.accept(this, args, msg);
-    }
-    
-    /**
-     * <p>Reads each file from {@code READ_FROM} and prints only the lines of each file that have
-     * more than one Location to a corresponding file in {@code WRITE_TO}.</p>
-     * @param op the Operation whose folders will be used
-     * @param args command-line args (not used)
-     * @param msg receives and handles messages output by arbitrary parts of this operation
-     */
-    private static void rmUniqIndeps(Operation op, String[] args, Consumer<String> msg) {
-        IntStream.range(IO.MIN_PHRASE_SIZE, IO.MAX_PHRASE_SIZE)
-                .parallel()
-                .forEach((i) -> {
-                    try(
-                            Scanner scan = new Scanner(new File(op.readFrom().filename(i)), IO.ENCODING); 
-                            OutputStreamWriter out  = IO.newOutputStreamWriter(
-                                        op.writeTo().filename(i), 
-                                        scan)){
-                        
-                        while(IO.scannerHasNonEmptyNextLine(scan)){
-                            String line = scan.nextLine();
-                            if(line.indexOf(Location.ELEMENT_DELIM) 
-                                    != line.lastIndexOf(Location.ELEMENT_DELIM)){
-                                //then there's multiple Locations on that line
-                                
-                                //the case of -1 == -1 can be ignored because a phrase 
-                                //with no Locations will not have been printed to file.
-                                out.write(line + IO.NEW_LINE);
-                            }
-                        }
-                        scan.close();
-                        out.close();
-                    } catch(IOException e){
-                    }
-                });
     }
     
     @FunctionalInterface
