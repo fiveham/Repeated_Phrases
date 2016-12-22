87278ea24bf8edf11a9b196ad05fac8f50d23cb9
fiveham <fiveham@users.noreply.github.com>
2016-12-21 23:51:11 -0500

    use Trail instead of List<TrailElement>

--- src/operate/DataManager.java
+++ src/operate/DataManager.java
@@ -9,14 +9,11 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import text.Chapter;
 import text.Location;
@@ -51,7 +48,7 @@ class DataManager {
         return softGet(chapters, this::generateChapters);
     }
     
-    public Collection<AnchorInfo> getAnchors(List<TrailElement> trail){
+    public Collection<AnchorInfo> getAnchors(Trail trail){
         return softGet(anchorData, () -> generateAnchorData(trail));
     }
     
@@ -88,7 +85,7 @@ class DataManager {
                 .collect(Collectors.toList());
     }
     
-    private void generateAnchorData(List<TrailElement> trail){
+    private void generateAnchorData(Trail trail){
         Collection<Chapter> chapters = getChapters();
         
         Map<Chapter, Collection<Quote>> allQuotes = Collections.synchronizedMap(new HashMap<>());
@@ -151,7 +148,7 @@ class DataManager {
 
     private static List<AnchorInfo> generateAnchorInfo(
             Map<Chapter, List<Quote>> diQuotes, 
-            List<TrailElement> trail){
+            Trail trail){
         
         List<AnchorInfo> result = new ArrayList<>();
 
@@ -176,161 +173,20 @@ class DataManager {
         return result;
     }
     
-    private static PhraseBox phrasesToLocations(Map<Chapter, List<Quote>> diQuotes, List<TrailElement> trail){
+    private static PhraseBox phrasesToLocations(Map<Chapter, List<Quote>> diQuotes, Trail trail){
         PhraseBox result = new PhraseBox();
         diQuotes.keySet().parallelStream().forEach(
                 (c) -> diQuotes.get(c).parallelStream().forEach(
                         (q) -> result.add(q.text(), q.location())));
         
-        //sort the anchors according to the trail file
-        /*Comparator<Location> phraseSorter = trailFile.exists() && trailFile.canRead() 
-                ? new AdHocComparator(trailFile) 
-                : Location::compareTo;*/
         for(String phrase : result.phrases()){
-            result.get(phrase).sort(new AdHocComparator(trail));
+            result.get(phrase).sort(trail);
         }
         
         return result;
     }
     
-    private static class AdHocComparator implements Comparator<Location>{
-        private final Map<String, Integer> chapterIndices;
-        
-        private AdHocComparator(List<TrailElement> elems){
-            chapterIndices = IntStream.range(0, elems.size())
-                    .mapToObj(Integer::valueOf)
-                    .collect(Collectors.toMap(
-                            (i) -> IO.stripFolderExtension(elems.get(i).focus()), 
-                            (i) -> i));
-        }
-        
-        @Override
-        public int compare(Location loc1, Location loc2){
-            String chapter1 = IO.stripFolderExtension(loc1.getFilename());
-            String chapter2 = IO.stripFolderExtension(loc2.getFilename());
-            
-            int indexInChapter1 = chapterIndices.get(chapter1);
-            int indexInChapter2 = chapterIndices.get(chapter2);
-            
-            return indexInChapter1 != indexInChapter2 
-                    ? indexInChapter1 - indexInChapter2 
-                    : loc1.getIndex() - loc2.getIndex();
-        }
-    }
-    
-    /**
-     * <p>Number of columns to anticipate in input file: {@value}</p> <p>The fourth column is unused
-     * at this time, but expecting it allows the third column to be cleanly isolated if a fourth
-     * column exists.</p>
-     */
-    private static final int COLUMN_COUNT = 4;
-    
-    /**
-     * <p>Returns a list of {@code TrailElement}s describing each chapter's predecessor and
-     * successor.</p>
-     * @param trailFilename the name of the trail-file from which trail data is extracted
-     * @return a list of {@code TrailElement}s describing each chapter's predecessor and successor
-     */
-    static List<TrailElement> getTrailElements(File trailFile){
-        return IO.fileContentsAsList(
-                trailFile, 
-                Scanner::nextLine, 
-                IO::scannerHasNonEmptyNextLine)
-                .stream()
-                .map((line) -> line.split("\t", COLUMN_COUNT))
-                .map(TrailElement::new)
-                .collect(Collectors.toList());
-    }
-    
-    //TODO can uses of List<TrailElement> be replaced with uses of a LinkedList?
-    //...using the nodes of the LinkedList to implicitly contain info about prev and next
-    /**
-     * <p>Represents an element of a chapter trail, a sequence of backward and forward links between
-     * chapters.</p>
-     */
-    static class TrailElement implements Comparable<TrailElement>{
-        
-        /**
-         * <p>The chapter to be linked as the preceding chapter in the trail.</p>
-         */
-        private final String prev;
-        
-        /**
-         * <p>The chapter for which links to the specified preceding and succeeding chapters are to
-         * be installed.</p>
-         */
-        private final String focus;
-        
-        /**
-         * <p>The chapter to be linked as the succeeding chapter in the trail.</p>
-         */
-        private final String next;
-        
-        TrailElement(String[] strings){
-            this(strings[INDEX_PREV], strings[INDEX_FOCUS], strings[INDEX_NEXT]);
-        }
-        
-        private static final int INDEX_PREV = 0;
-        private static final int INDEX_FOCUS = 1;
-        private static final int INDEX_NEXT = 2;
-        
-        /**
-         * <p>Constructs a TrailElement indicating that the chapter named by {@code focus} has the
-         * chapter named by {@code next} as its successor.</p>
-         * @param prev the chapter before {@code focus} in sequence
-         * @param focus the chapter in which links to {@code prev} and {@code next} are to be
-         * installed
-         * @param next the chapter after {@code focus} in sequence
-         */
-        TrailElement(String prev, String focus, String next){
-            this.prev = prev;
-            this.focus = focus;
-            this.next = next;
-        }
-        
-        /**
-         * <p>Compares two TrailElements, first by their {@code focus}, then by their {@code prev},
-         * and last by their {@code next}.</p>
-         * @return an int whose sign reflects the natural ordering between this TrailElement and
-         * {@code t}
-         */
-        @Override
-        public int compareTo(TrailElement t){
-            int comp = focus.compareTo(t.focus);
-            if(comp != 0){
-                return comp;
-            } else if(0 != (comp = prev.compareTo(t.prev))){
-                return comp;
-            } else{
-                return next.compareTo(t.next);
-            }
-        }
-        
-        /**
-         * <p>Returns {@link #prev prev}.</p>
-         * @return {@link #prev prev}
-         */
-        public String prev(){
-            return prev;
-        }
-        
-        /**
-         * <p>Returns {@link #focus focus}.</p>
-         * @return {@link #focus focus}
-         */
-        public String focus(){
-            return focus;
-        }
-        
-        /**
-         * <p>Returns {@link #next next}.</p>
-         * @return {@link #next next}
-         */
-        public String next(){
-            return next;
-        }
-    }
-    
+    //TODO
     private void generateLinkedChapters(){
         
     }
