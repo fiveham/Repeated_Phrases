9ac63d54c72300901566fb8a2481a5a96402c4d3
fiveham <fiveham@users.noreply.github.com>
2017-01-02 11:53:57 -0500

    2-space indents

--- src/html/CharLiteral.java
+++ src/html/CharLiteral.java
@@ -12,74 +12,74 @@ import text.Phrase;
  * {@link CharCode character codes}.</p>
  */
 public class CharLiteral extends HtmlEntity {
-    
-    public static final CharLiteral RIGHT_SINGLE_QUOTE = new CharLiteral(Files.RIGHT_SINGLE_QUOTE);
-    public static final CharLiteral APOSTROPHE = new CharLiteral('\'');
-    
-    /**
-     * <p>The newline character '\n'.</p>
-     */
-    public static final char NEW_LINE = '\n';
-    
-    public static final List<CharLiteral> NEW_LINE_LITERAL = 
-            Collections.unmodifiableList(asList(Files.NEW_LINE));
-	
-    /**
-     * <p>The literal character this object wraps.</p>
-     */
+  
+  public static final CharLiteral RIGHT_SINGLE_QUOTE = new CharLiteral(Files.RIGHT_SINGLE_QUOTE);
+  public static final CharLiteral APOSTROPHE = new CharLiteral('\'');
+  
+  /**
+   * <p>The newline character '\n'.</p>
+   */
+  public static final char NEW_LINE = '\n';
+  
+  public static final List<CharLiteral> NEW_LINE_LITERAL = 
+          Collections.unmodifiableList(asList(Files.NEW_LINE));
+
+  /**
+   * <p>The literal character this object wraps.</p>
+   */
 	public final char c;
 	
-    /**
-     * <p>Constructs a Ch wrapping the specified literal character {@code c}.</p>
-     * @param c the literal character to wrap as an {@code HTMLEntity}.
-     */
+  /**
+   * <p>Constructs a Ch wrapping the specified literal character {@code c}.</p>
+   * @param c the literal character to wrap as an {@code HTMLEntity}.
+   */
 	public CharLiteral(char c) {
 		this.c = c;
 	}
-	
-    public static boolean is1(HtmlEntity h){
-        return CharLiteral.class.isInstance(h) && ((CharLiteral)h).c == '1';
-    }
-    
+
+  public static boolean is1(HtmlEntity h){
+      return CharLiteral.class.isInstance(h) && ((CharLiteral)h).c == '1';
+  }
+  
 	@Override
-    /**
-     * <p>Returns a string representation of this Ch. If the wrapped literal character is a newline
-     * ('\n'), then a system-dependent newline is returned (via
-     * {@code System.getProperty("line.separator")}), otherwise returns a string consisting of
-     * exactly the wrapped literal character {@code c}.</p>
-     * @return a string representation of this Ch: the literal wrapped character or a
-     * system-dependent newline if the wrapped character is '\n'.
-     */
+  /**
+   * <p>Returns a string representation of this Ch. If the wrapped literal character is a newline
+   * ('\n'), then a system-dependent newline is returned (via
+   * {@code System.getProperty("line.separator")}), otherwise returns a string consisting of
+   * exactly the wrapped literal character {@code c}.</p>
+   * @return a string representation of this Ch: the literal wrapped character or a
+   * system-dependent newline if the wrapped character is '\n'.
+   */
 	public String toString(){
 		return (c == NEW_LINE) 
-		        ? System.getProperty("line.separator") 
-		        : new String(new char[]{c});
+        ? System.getProperty("line.separator") 
+        : new String(new char[]{c});
 	}
 	
 	public static List<CharLiteral> asList(String s){
-	    List<CharLiteral> result = new ArrayList<>(s.length());
-	    for(int i = 0; i < s.length(); i++){
-	        result.add(new CharLiteral(s.charAt(i)));
-	    }
-	    return result;
+    List<CharLiteral> result = new ArrayList<>(s.length());
+    for(int i = 0; i < s.length(); i++){
+      result.add(new CharLiteral(s.charAt(i)));
+    }
+    return result;
 	}
 	
 	@Override
-    /**
-     * <p>Returns true if {@code o} is a {@code Ch} and wraps the name character as this Ch, false
-     * otherwise.</p>
-     * @return true if {@code o} is a {@code Ch} and wraps the name character as this Ch, false
-     * otherwise.
-     */
+  /**
+   * <p>Returns true if {@code o} is a {@code Ch} and wraps the name character as this Ch, false
+   * otherwise.</p>
+   * @return true if {@code o} is a {@code Ch} and wraps the name character as this Ch, false
+   * otherwise.
+   */
 	public boolean equals(Object o){
 		return o instanceof CharLiteral && c == ((CharLiteral)o).c;
 	}
 	
 	@Override
-    /**
-     * <p>Returns a plaintext representation of this Ch: the result returned by
-     * {@code toString()}.</p>
-     */
+  /**
+   * <p>Returns a plaintext representation of this Ch: the result returned by
+   * {@code toString()}.</p>
+   */
 	public String txtString(){
 		return toString();
 	}
@@ -91,6 +91,6 @@ public class CharLiteral extends HtmlEntity {
 	
 	@Override
 	public boolean match(char c){
-	    return c == this.c;
+    return c == this.c;
 	}
 }
